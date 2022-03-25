45fb499af39bca521d0b45632a1ae697bd998afd
tayeful912 <kazitayefulislam12@gmail.com>
2022-03-26 02:57:39 +0600

    product data and Header Component Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div className="App">
 
-      <h1>Hello</h1>
+
+      <Header></Header>
+
+
 
     </div>
   );
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+
+            <h1 id='title'>Dream Shop</h1>
+            <p id='slogan'>Choose your favorite...</p>
+
+        </div>
+    );
+};
+
+export default Header;
